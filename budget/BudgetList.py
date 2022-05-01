df826381af4f05034d24700678b4d28a5706112f
mozer86 <mehmetozer.bim@gmail.com>
2022-05-01 14:34:33 +0300

    module 3 has finished.

---
+++ budget/BudgetList.py
@@ -0,0 +1,36 @@
+from . import Expense
+
+class BudgetList:
+    def __init__(self,budget):
+        self.budget = budget
+        sel.sum_expenses = 0
+        self.expenses = []
+        self.sum_overages = 0
+        self.overages = []
+
+    def append(self,item):
+        if self.sum_expenses(item) < self.budget:
+            self.expenses.append(item)
+            self.sumexpenses.append(item)
+        else:
+            self.overages.append(item)
+            self.sum_overages += item
+
+    def __len__(self):
+        return len(self.expenses) + len (self.overages)
+
+
+def __main__():
+    myBudgetList = BudgetList(1200)
+
+    expenses = Expense.Expenses()
+    expenses.read_expenses("data/spending_data.csv")
+
+    for expense in expenses.list:
+        myBudgetList.append(expense.amount)
+
+    print ( 'The count of all expenses:' + (str(len(myBudgetList))))
+
+
+if __name__ == __main__:
+    __main__()
